e55b9f780caab6b47529f094a38c04b8395f90f1
dsilvaEVG <dsilvavega.guadalupe@alumnado.fundacionloyola.net>
2025-01-21 11:38:17 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,44 @@
  <?php 
-    require_once('fpdf.php');
-//omito la conexión por seguridad, se realizaron las pruebas en el hosting de la clase, en la bbdd del juego
-define("SERVIDOR", ""); // DATOS SERVIDOR
-define("USUARIO", "");  // DATOS USUARIO BBDD
-define("PASSWORD", ""); // DATOS CONTRASEÑA BBDD
-define("BBDD", ""); // NOMBRE BBDD
+    require_once('conectarBBDD2.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de Ranking</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <header><h1>Ranking</h1></header>
+    <main>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Puntos</th>
+                <th>Num. Fallos</th>
+                <th>Tiempo</th>
+            </tr>
+            <?php 
+             while ($fila = $resultado->fetch_assoc()) {
+            echo "<tr>
+                    <td>".$fila["nombre"]."</td>
+                    <td>".$fila["puntos"]."</td>
+                    <td>".$fila["numFallos"]."</td>
+                    <td>".$fila["tiempo"]."</td> 
+            </tr>";
+     
+             } 
+            ?>
+        </table>
+        <form action="exportarPdf.php" method="get">
+            <button type="submit" class="button">Exportar a PDF</button>
+        </form>
+    </main>
+</body>
+</html>
+<?php
 
-    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
-    $conexion->set_charset("utf8"); 
-    $controlador = new mysqli_driver();
-    $controlador->report_mode = MYSQLI_REPORT_OFF;
-
-    $sql = "SELECT nombre, puntos, numFallos, tiempo FROM puntuacion 
-    WHERE idContinente = 4
-	ORDER BY puntos DESC, tiempo
-    LIMIT 25";
-
-    $resultado = $conexion->query($sql); 
-    
-    $pdf = new FPDF();
-    $pdf->AddPage();
-    $pdf->SetFont('Arial','B',12);
-
-    $pdf->Cell(0, 10, 'Ranking', 0, 1, 'C');
-    $pdf->Ln(10);
-
-    $ancho1 = 50;
-    $ancho2 = 30;
-    $ancho3 = 40;
-    $ancho4 = 30;
-
-    $anchoTotal = $ancho1 + $ancho2 + $ancho3 + $ancho4;
-    $posX = (210 - $anchoTotal) / 2;
-
-    $pdf->SetX($posX);
-
-    $pdf->Cell($ancho1, 10, 'Nombre', 1, 0, 'C');
-    $pdf->Cell($ancho2, 10, 'Puntos', 1, 0, 'C');
-    $pdf->Cell($ancho3, 10, 'Num. Fallos', 1, 0, 'C');
-    $pdf->Cell($ancho4, 10, 'Tiempo', 1, 1, 'C');
-
-    $pdf->SetFont('Arial', '', 10);
-
-    while ($fila = $resultado->fetch_assoc()) {
-        $pdf->SetX($posX);
-        $pdf->Cell($ancho1, 10, $fila['nombre'], 1, 0, 'C');
-        $pdf->Cell($ancho2, 10, $fila['puntos'], 1, 0, 'C');
-        $pdf->Cell($ancho3, 10, $fila['numFallos'], 1, 0, 'C');
-        $pdf->Cell($ancho4, 10, $fila['tiempo'], 1, 1, 'C');
-    }
 $conexion->close();
-    $pdf->Output('I');
     
 ?>
